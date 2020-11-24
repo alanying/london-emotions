25311107da4e38d21ad17cc98b70849edd19e7a7
Luke Moberly <lukemoberly@gmail.com>
2020-11-24 16:54:09 +0000

    Add time tracker decorator

--- LondonEmotions/trainer.py
+++ LondonEmotions/trainer.py
@@ -1,3 +1,5 @@
+from LondonEmotions.utils import simple_time_tracker
+
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.naive_bayes import MultinomialNB
